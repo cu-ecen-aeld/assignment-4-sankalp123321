456ac990e94bdaaf5df628e370bb608602c45f9a
Sankalp Agrawal <43979892+sankalp123321@users.noreply.github.com>
2022-02-06 22:17:45 -0700

    chore: updated makefile

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '180f88eced8c00f5cbb885c21930f1b7bb68b44e'
+AESD_ASSIGNMENTS_VERSION = '543f7d39d33a4e418526046ec3f763dce253012a'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
